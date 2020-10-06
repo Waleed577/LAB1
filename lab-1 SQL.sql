1f537e48143a31544d015bc7a2bf39bcd9415de2
Muhammad Waleed <55361003+Waleed577@users.noreply.github.com>
2020-10-06 19:25:59 +0500

    Update lab-1 SQL.sql

--- lab-1 SQL.sql
+++ lab-1 SQL.sql
@@ -1,3 +1,6 @@
+
+//Lab related to understanding basic concepts of creating tables, inserting data into tables in database 
+
 create database lab2
 use lab2
 create table student (
@@ -10,6 +13,7 @@ insert into student values('1', 'zahid ali', 'uet lahore', 23)
 insert into student values('2', 'Ahmad bilal', 'uet lahore', 122)
 insert into student values('3', 'abdul Samad', 'uet lahore', 13)
 insert into student values('4', 'Abdullah safdar', 'uet lahore', 2)
+insert into student values('5', 'Rafaay', 'uet lahore', 141)
 
 select * from student
 
@@ -20,7 +24,7 @@ update student set student_name='Zahdi Ali' where (cnic='1')
 update student set student_name='Abdul Samad', age='19' where (cnic='3')
 update student set student_address = 'ZAhid ali2' where cnic = '1'
 
-delete from student where cnic = '3'
+delete from student where cnic = '3'  // syntax of delete query
 
 create table subject_result(
 	course_code char (13),
@@ -29,11 +33,13 @@ create table subject_result(
 	marks int,
 	student_roll  char(13) foreign key references student(cnic)
 )
-insert into subject_result (course_code, course_title, student_grade, marks) values('11', 'zahid ali', 'uet lahore', 23)
-insert into subject_result (course_code, course_title, student_grade, marks) values('12', 'Ahmad bilal', 'uet lahore', 122)
-insert into subject_result (course_code, course_title, student_grade, marks) values('13', 'abdul Samad', 'uet lahore', 13)
+insert into subject_result (course_code, course_title, student_grade, marks) values('CS121', 'Database', 'A+', 90)
+insert into subject_result (course_code, course_title, student_grade, marks) values('CS311', 'AOA', 'A-', 85)
+insert into subject_result (course_code, course_title, student_grade, marks) values('CS308', 'OS', 'B+', 70)
+insert into subject_result (course_code, course_title, student_grade, marks) values('CS201', 'COAL', 'B', 61)
+insert into subject_result (course_code, course_title, student_grade, marks) values('CS113', 'DLD', 'A', 86)
 
-truncate table subject_result
+truncate table subject_result  // syntax of truncate
 select * from subject_result
 
 
